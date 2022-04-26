a658af0c165d2b7885f99aa7b6e297f93d530701
g11tech <76567250+g11tech@users.noreply.github.com>
2022-04-26 15:16:07 +0530

    fix: compressor didn't chunkify big payload (#3)
    * Update the compressor to chunk the bytes as per the standard
    
    * bump package version
    
    * test for multi chunk compress
    
    * remove the dupilicated test
    
    * revering version bump as releases are auto created
    
    * benchmarks
    
    * log unhandled exception
    
    * remove self

--- test/compress-test.js
+++ test/compress-test.js
@@ -1,9 +1,15 @@
 var spawn = require('child_process').spawn
-
   , createCompressStream = require('../').createCompressStream
   , test = require('tap').test
   , largerInput = require('fs').readFileSync(__filename)
   , largerInputString = largerInput.toString()
+
+const UNCOMPRESSED_CHUNK_SIZE = 65536
+let superLargeInput = largerInput;
+for (let i = largerInput.length; i <= UNCOMPRESSED_CHUNK_SIZE; i += largerInput.length) {
+  superLargeInput = Buffer.concat([superLargeInput, largerInput]);
+}
+const superLargeInputString = superLargeInput.toString();
 
 test('compress small string', function (t) {
   var child = spawn('python', [ '-m', 'snappy', '-d' ])
@@ -46,5 +52,28 @@ test('compress large string', function (t) {
   compressStream.pipe(child.stdin)
 
   compressStream.write(largerInputString)
+  compressStream.end()
+})
+
+
+test('compress very very large string', function (t) {
+  var child = spawn('python', [ '-m', 'snappy', '-d' ])
+    , compressStream = createCompressStream()
+    , data = ''
+
+  child.stdout.on('data', function (chunk) {
+    data = data + chunk.toString()
+  })
+
+  child.stdout.on('end', function () {
+    t.equal(data, superLargeInputString)
+    t.end()
+  })
+
+  child.stderr.pipe(process.stderr)
+
+  compressStream.pipe(child.stdin)
+
+  compressStream.write(superLargeInputString)
   compressStream.end()
 })
